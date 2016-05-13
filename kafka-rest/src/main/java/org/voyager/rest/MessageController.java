bc469939a8be541d6bb6126936d9f11321dafe4a
scottlepp <scott.lepper@gmail.com>
2016-05-13 08:14:01 -0400

    added ip

--- kafka-rest/src/main/java/org/voyager/rest/MessageController.java
+++ kafka-rest/src/main/java/org/voyager/rest/MessageController.java
@@ -4,18 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.io.Resources;
-
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
-import kafka.producer.ProducerConfig;
 
 @RestController
 public class MessageController {
@@ -33,10 +32,14 @@ public class MessageController {
 	}
 	
     @RequestMapping(value="/message", method=RequestMethod.POST)
-    public void send(@RequestParam(value="message") String message) {
-    	ProducerRecord<String, String> data = new ProducerRecord<String, String>("user", "log", message);
+    public void send(@RequestParam(value="message") String message, HttpServletRequest request) {
     	
-    	//KeyedMessage<String, String> data = new KeyedMessage<String, String>("user", "log", message);
+    	JSONObject json = new JSONObject(message);
+    	json.append("ip", request.getRemoteAddr());
+//    	json.append("ip", "172.217.2.196");
+    	
+    	ProducerRecord<String, String> data = new ProducerRecord<String, String>("user", "log", json.toString());
+    	
     	producer.send(data);
     }
 }
